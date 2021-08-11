4a72d0f220100d717d7ecf5960846aee2c65c75b
hiagopsilva <hiagohps360@gmail.com>
2021-08-11 18:23:47 -0300

    feat: scene sign in

--- src/scenes/SignIn/index.tsx
+++ src/scenes/SignIn/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Image, View } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png';
+import { Wrapper, Content, Image, Title, SubTitle } from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <View>
-      <Image source={IllustrationImg} />
-    </View>
+    <Wrapper>
+      <Image source={IllustrationImg} resizeMode="stretch" />
+
+      <Content>
+        <Title>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Title>
+
+        <SubTitle>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </SubTitle>
+      </Content>
+    </Wrapper>
   );
 };
 
--- src/scenes/SignIn/styles.ts
+++ src/scenes/SignIn/styles.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components/native';
+import { colors } from '../../global/styles/theme';
 
-export const Container = styled.View`
-  
+export const Wrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.secondary90};
+`;
+
+export const Content = styled.View`
+  margin-top: -40px;
+`;
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 360px;
+`;
+
+export const Title = styled.Text`
+  color: ${colors.heading};
+  text-align: center;
+  font-size: 40px;
+  margin-bottom: 16px;
+`;
+
+export const SubTitle = styled.Text`
+  color: ${colors.heading};
+  font-size: 15px;
+  text-align: center;
+  margin-bottom: 64px;
 `;
