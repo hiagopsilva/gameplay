78afb92d8128525d5b8b4621c7e2e085a0f15a3b
hiagopsilva <hiagohps360@gmail.com>
2021-08-11 18:42:36 -0300

    chore: props touchable opacity

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { TouchableOpacityProps } from 'react-native';
 
 import { Wrapper, Text, Image, Container } from './styles';
 import DiscordImg from '../../assets/discord.png';
 
-type Props = {
+type Props = TouchableOpacityProps & {
   title: string;
 };
 
-const ButtonIcon: React.FC<Props> = ({ title }) => {
+const ButtonIcon: React.FC<Props> = ({ title, ...rest }) => {
   return (
-    <Container>
+    <Container {...rest}>
       <Wrapper>
         <Image source={DiscordImg} />
       </Wrapper>
