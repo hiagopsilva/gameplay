1c57007ac27b23c59b28ea6560a07dde993209c0
hiagopsilva <hiagohps360@gmail.com>
2021-08-11 17:47:39 -0300

    Initial commit

---
+++ src/scenes/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return <View><Text>SignIn</Text></View>
+}
+
+export { SignIn };
