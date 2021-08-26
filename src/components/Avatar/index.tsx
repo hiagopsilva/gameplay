971567fb0342e308af37a37562cbbec12a474687
hiagopsilva <hiagohps360@gmail.com>
2021-08-25 21:02:15 -0300

    feat: avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Container, LinearGradient, Image } from './styles';
+import Logo from '../../assets/banner.png';
+
+type Props = {
+  urlImage?: string;
+};
+
+const Avatar: React.FC<Props> = urlImage => {
+  return (
+    <Container>
+      <LinearGradient>
+        <Image source={Logo} />
+      </LinearGradient>
+    </Container>
+  );
+};
+
+export default Avatar;
---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+import { LinearGradient as LinearGradientModules } from 'expo-linear-gradient';
+import { colors } from '../../global/styles/theme';
+
+export const Container = styled.View`
+  width: 53px;
+  height: 53px;
+  border-radius: 8px;
+  align-items: center;
+  justify-content: center;
+  margin-right: 22px;
+`;
+
+export const LinearGradient = styled(LinearGradientModules).attrs({
+  colors: [colors.secondary50, colors.secondary70],
+})`
+  flex: 1;
+  border-radius: 8px;
+`;
+
+export const Image = styled.Image`
+  width: 46px;
+  height: 46px;
+  border-radius: 8px;
+`;
