faadc8ca7022a2a26defbc8831df8c4a5012b3c9
hiagopsilva <hiagohps360@gmail.com>
2021-08-26 02:50:15 -0300

    feat: categry list

--- src/scenes/CategorySelect/index.tsx
+++ src/scenes/CategorySelect/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
+import { categories } from '../../utils/categories';
+import { Category } from '../../components/Category';
 
-const CategorySelect: React.FC = () => {
-  return <View />;
+type Props = {
+  categorySelected: string;
+  setCategory: (categoryId: string) => void;
+};
+
+const CategorySelect: React.FC<Props> = ({ categorySelected, setCategory }) => {
+  return (
+    <Container>
+      {categories.map(category => (
+        <Category
+          key={category.id}
+          title={category.title}
+          icon={category.icon}
+          checked={category.id === categorySelected}
+          onPress={() => setCategory(category.id)}
+        />
+      ))}
+    </Container>
+  );
 };
 
 export { CategorySelect };
--- src/scenes/CategorySelect/styles.ts
+++ src/scenes/CategorySelect/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingRight: 40 },
+})`
+  flex-direction: row;
+  min-height: 120px;
+  min-width: 120px;
+  padding-left: 24px;
+`;
