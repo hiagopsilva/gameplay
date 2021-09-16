ed92d7e12a3c305f934368ef6683b8951a5fa764
hiagopsilva <hiagohps360@gmail.com>
2021-09-16 13:42:37 -0300

    fix: image

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -5,7 +5,10 @@ import { Container } from './styles';
 import Logo from '../../assets/discord.svg';
 
 const GuildIcon: React.FC = ({ ...rest }) => {
-  return <Container {...rest} uri={Logo} />;
+  const uri =
+    'https://images-na.ssl-images-amazon.com/images/I/51lpm9SpsJL.png';
+
+  return <Container {...rest} source={{ uri }} />;
 };
 
 export { GuildIcon };
