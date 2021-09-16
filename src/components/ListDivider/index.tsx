cedfce710c73fe05854a925fb6385e9f9ea306d8
hiagopsilva <hiagohps360@gmail.com>
2021-09-16 14:03:40 -0300

    feat: divider component

---
+++ src/components/ListDivider/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ListDivider: React.FC = () => {
+  return <Container />;
+};
+
+export default ListDivider;
---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import { colors } from '../../global/styles/theme';
+
+export const Container = styled.View`
+  width: 80%;
+  height: 1px;
+  background-color: ${colors.secondary40};
+  margin-vertical: 21px;
+  align-self: flex-end;
+`;
